b88829579f28d3593ae81b83c342e4cb9fa8ef4b
avivmoshe11 <avivmoshe123@gmail.com>
2025-03-24 19:43:43 +0200

    chore: initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,10 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
 ]);
