88494ab3c938bbe84571238a79f27be38ca4e12a
avivmoshe11 <avivmoshe123@gmail.com>
2025-06-04 19:16:02 +0300

    test

--- Utilities/logger.utilities.ts
+++ Utilities/logger.utilities.ts
@@ -19,7 +19,7 @@ export class LoggerUtilities {
     embed.setTitle(title);
     embed.setFooter({
       text: `Made by the development team`,
-      iconURL: "https://cdn.discordapp.com/icons/1369668963021099131/578c1fa7db06a857a2c25c91191fb594.webp?size=80&quality=lossless"
+      iconURL: "https://cdn.discordapp.com/icons/1369668963021099131/578c1fa7db06a857a2c25c91191fb594.webp"
     });
 
     try {
