a4d8f915a0e2c6cfba8b9003539137f6de020731
avivmoshe11 <avivmoshe123@gmail.com>
2025-07-14 21:49:36 +0300

    Added restriction durations

--- Modules/Moderation/Config/restriction.config.ts
+++ Modules/Moderation/Config/restriction.config.ts
@@ -14,17 +14,25 @@ export enum RestrictionDurations {
   OneHour = 60,
   ThreeHours = 180,
   SixHours = 360,
+  TwelveHours = 720,
+  OneDay = 1440,
+  ThreeDays = 4320,
+  OneWeek = 10080,
   Permanent = -1
 }
 
 export const muteDurations = [
-  { name: "1 Min", value: RestrictionDurations.OneMinute },
+  // { name: "1 Min", value: RestrictionDurations.OneMinute },
   { name: "5 Min", value: RestrictionDurations.FiveMinutes },
   { name: "15 Min", value: RestrictionDurations.FifteenMinutes },
   { name: "30 Min", value: RestrictionDurations.ThirtyMinutes },
   { name: "1 Hour", value: RestrictionDurations.OneHour },
-  { name: "3 Hour", value: RestrictionDurations.ThreeHours },
-  { name: "6 Hour", value: RestrictionDurations.SixHours },
+  { name: "3 Hours", value: RestrictionDurations.ThreeHours },
+  { name: "6 Hours", value: RestrictionDurations.SixHours },
+  { name: "12 Hours", value: RestrictionDurations.TwelveHours },
+  { name: "1 Day", value: RestrictionDurations.OneDay },
+  { name: "3 Days", value: RestrictionDurations.ThreeDays },
+  { name: "1 Week", value: RestrictionDurations.OneWeek },
   { name: "Permanent", value: RestrictionDurations.Permanent }
 ];
 
