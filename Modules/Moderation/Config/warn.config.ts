be2caa86d6c56b289698e1316cdcb76f28a742fb
avivmoshe11 <avivmoshe123@gmail.com>
2025-07-16 00:14:55 +0300

    Changed warn restrictions

--- Modules/Moderation/Config/warn.config.ts
+++ Modules/Moderation/Config/warn.config.ts
@@ -1,9 +1,11 @@
 import { RestrictionDurations } from "./restriction.config.js";
 
 export const warnAmount = new Map([
-  [3, RestrictionDurations.ThirtyMinutes],
-  [4, RestrictionDurations.OneHour],
-  [5, RestrictionDurations.ThreeHours]
+  [3, RestrictionDurations.SixHours],
+  [4, RestrictionDurations.TwelveHours],
+  [5, RestrictionDurations.OneDay],
+  [6, RestrictionDurations.ThreeDays],
+  [7, RestrictionDurations.OneWeek]
 ]);
 
 export const maxWarningThreshHold = Math.max(...warnAmount.keys());
