06be42971052f9f6d7d36198ccfdb01d8b69de1c
avivmoshe11 <avivmoshe123@gmail.com>
2025-10-15 15:32:06 +0300

    Prevent dms in stage mode

--- Utilities/direct-message.utilities.ts
+++ Utilities/direct-message.utilities.ts
@@ -2,6 +2,8 @@ import { MessageCreateOptions, MessagePayload, User } from "discord.js";
 
 export class DirectMessageUtilities {
   public static async sendDM(target: User, options: string | MessagePayload | MessageCreateOptions) {
+    if (process.env.NODE_ENV === "stage") return;
+
     let informed = false;
     try {
       const dmChannel = target.dmChannel || (await target.createDM(true));
